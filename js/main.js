4925eed6e278cc361b3a5d118a987de3a12c7f29
Katie Sexton <ksexton@hugeinc.com>
2016-04-27 00:00:34 -0400

    Add selectable font colors

--- js/main.js
+++ js/main.js
@@ -7,12 +7,15 @@ var AAG = (function() {
     var albumArtist = document.getElementById('albumArtist');
     var albumSubtitle = document.getElementById('albumSubtitle');
     var albumInfoSubmit = document.getElementById('submit-text');
+    var palette = document.getElementById('palette-options');
 
     var colorScheme;
     var contrastColor;
+    var selectedColor;
 
     albumInfoSubmit.addEventListener("click", getSubmittedText, false);
     imageLoader.addEventListener('change', handleImage, false);
+    palette.addEventListener('click', changeDefaultColor, false);
 
     function handleImage(e){
         var reader = new FileReader();
@@ -21,14 +24,15 @@ var AAG = (function() {
             img.onload = function(){
                 newSize = computeNewSize(img);
                 var colorThief = new ColorThief();
-                colorScheme = colorThief.getPalette(img, 3);
+                colorScheme = colorThief.getPalette(img, 6);
+                drawPaletteOptions(colorScheme);   
                 canvas.width = 725;
                 canvas.height = 725;
                 ctx.drawImage(img, 0, 0, newSize.width, newSize.height);
             }
             img.src = event.target.result;
         }
-        reader.readAsDataURL(e.target.files[0]);     
+        reader.readAsDataURL(e.target.files[0]);  
     }
 
     function computeNewSize(img){
@@ -56,7 +60,29 @@ var AAG = (function() {
 
     }
 
-    function getRgbString() {
+    // function computeContrastColor(rgb) {
+
+    // }
+
+    function drawPaletteOptions(colorScheme) {
+        //var templ = _.template('<div class="option" style="background-color: rgb('<%= color %>');"></div>');
+        markup = '';
+        _.each(colorScheme, function(scheme){
+            markup += '<a class="option" style="background-color: rgb(' + scheme[0] + ', ' + scheme[1] + ',' + scheme[2] + ');"></a>';
+        });
+        palette.innerHTML = markup;  
+
+    }
+
+    function changeDefaultColor(e) {
+        console.log(e)
+        if (e.target && e.target.matches("a.option")) {
+            color = e.target.style.backgroundColor;
+            selectedColor = color;
+        }
+    }
+
+    function getRgbString(arr) {
         var rgb;
         rgb = "rgb(" + colorScheme[1][0] + ","+ colorScheme[1][1] +"," + colorScheme[1][2] + ")";
         console.log(rgb);
@@ -66,21 +92,21 @@ var AAG = (function() {
     function drawArtist(input){
         ctx.font = "bold 42px Libre Baskerville";
         ctx.textAlign = "end";
-        ctx.fillStyle = getRgbString();
+        ctx.fillStyle = selectedColor || getRgbString();
         ctx.fillText(input, 685, 60);
     }
 
     function drawSubtitle(input){
         ctx.font = "300 14px Libre Baskerville";
         ctx.textAlign = "end";
-        ctx.fillStyle = getRgbString();
+        ctx.fillStyle = selectedColor || getRgbString();
         ctx.fillText(input, 685, 90);   
     }
 
     function drawTitle(input) {
         ctx.font = "200 italic 32px Libre Baskerville";
         ctx.textAlign = "end";
-        ctx.fillStyle = getRgbString();
+        ctx.fillStyle = selectedColor || getRgbString();
         ctx.fillText(input, 685, 140);
     }
 
